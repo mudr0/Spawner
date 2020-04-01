8d257922b8e947eb821ae4c73c92c4fa36c71f81
Lev <themostregular@gmail.com>
2020-04-01 18:03:24 +0700

    init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _secondsBetweenSpawn;
+
+    private float _passedSeconds;
+
+    private void Update()
+    {
+        _passedSeconds += Time.deltaTime;
+
+        if (_passedSeconds >= _secondsBetweenSpawn)
+        {
+            _passedSeconds = 0;
+            int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+            Instantiate(_enemyPrefab, _spawnPoints[spawnPointNumber]);
+        }
+    }
+}
